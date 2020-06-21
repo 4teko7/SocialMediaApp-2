2caab710579c52694ee1cef7bbbc06cc4e273d90
4teko7 <bilal.tekin@boun.edu.tr>
2020-06-21 23:41:41 +0300

    expressWasAddedAndMethodsWereRearranged

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,41 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const app = admin.initializeApp();
+admin.initializeApp();
 
+const express = require('express');
+const app = express();
 
+// https://baseurl.com/api/screams  = get
+app.get('/screams',(req,res)=>{
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World !");
+    admin.firestore().collection('screams')
+    .orderBy('createdAt','desc')
+    .get()
+    .then(data => {
+        let screams = []
+        data.forEach(doc => {
+            screams.push({
+                screamId: doc.id,
+                ...doc.data()
+            })
+        });
+        return res.json(screams);
+    })
+    .catch(err=>console.error(err));
+
 });
 
-
-exports.getScreams = functions.https.onRequest((req,res) =>{
-    admin.firestore().collection('screams').get()
-        .then(data => {
-            let screams = []
-            data.forEach(doc => {
-                screams.push(doc.data())
-            });
-            return res.json(screams);
-        })
-        .catch(err=>console.error(err));
-});
-
-
-exports.createScreams = functions.https.onRequest((req,res) =>{
+// https://baseurl.com/api/screams  = post
+app.post('/screams',(req,res) =>{
+   
     if(req.method !== "POST") {
         res.status(400).json({error : "method are not allowed !"});
     }
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
     admin.firestore()
         .collection('screams')
@@ -41,5 +46,13 @@ exports.createScreams = functions.https.onRequest((req,res) =>{
         .catch(err =>{
             res.status(500).json({error:"something went wrong..."})
             console.error(err);
-        })
-})
+        });
+
+});
+
+
+// https://baseurl.com/api/
+
+exports.api = functions.region('europe-west1').https.onRequest(app);  // If you want the machine region be europe-west1
+
+// exports.api = functions.https.onRequest(app); 
