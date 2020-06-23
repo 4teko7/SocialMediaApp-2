f60334d1f1a864b13fffd8c5fda438da38855ee4
4teko7 <bilal.tekin@boun.edu.tr>
2020-06-24 00:03:18 +0300

    timeWasMadeRelativeWithDayJS

--- ReactPart/react-firebase-social-media-app/src/components/Scream.js
+++ ReactPart/react-firebase-social-media-app/src/components/Scream.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import withStyles from "@material-ui/core/styles/withStyles"
-
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 //React Library
 import { Link } from 'react-router-dom';
@@ -30,6 +31,7 @@ const styles = {
 
 class Scream extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const {classes,scream : {body,createdAt,userImage,userHandle,screamId,likeCount,commentCount}} = this.props;
         console.log("classes : " , classes);
         return (
@@ -45,7 +47,7 @@ class Scream extends Component {
                     to={`/users/${userHandle}`}
                     color="primary"
                     >{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>
 
