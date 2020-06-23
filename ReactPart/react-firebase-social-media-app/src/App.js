46b2f7aa287137ddd004e62e314c2981946cec5b
4teko7 <bilal.tekin@boun.edu.tr>
2020-06-23 20:06:06 +0300

    materialUiThemeProviderWithCustomThemeWasAdded

--- ReactPart/react-firebase-social-media-app/src/App.js
+++ ReactPart/react-firebase-social-media-app/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+//React Library
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+//MaterialUi
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
 
 //components
 import Navbar from "./components/Navbar"
@@ -11,22 +18,39 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Signup from './pages/signup'
 
-
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      light:"#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary:{
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-          <Navbar />
-          <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/signup' component={Signup} />
-              </Switch>
-          </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+              <Navbar />
+              <div className="container">
+                  <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/signup' component={Signup} />
+                  </Switch>
+              </div>
+          </Router>
+        </div>
+    </MuiThemeProvider>
   );
 }
 
